2a82f90cd0f2b6b7529d413209720cc09f9ed23a
vinicius <vmm_7@hotmail.com>
2022-12-11 12:55:21 -0300

    Exemplo de 'namespaces' e cópias

---
+++ PessoaCopia/Pessoa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemplosFundamentos.PessoaCopia
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá! O meu nome é {Nome}, e tenho {Idade} anos de idade.");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿/* assim: */
+// using ExemplosFundamentos.Models;
+// Pessoa p = new Pessoa();
+
+/* ou assim: */
+ExemplosFundamentos.Models.Pessoa p = new ExemplosFundamentos.Models.Pessoa();
