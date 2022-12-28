3104cbe6790de5e7acbdf9cc55e2bb64631a7e1e
vinicius <vmm_7@hotmail.com>
2022-12-28 19:53:47 -0300

    Tipos de variável e alguns detalhes

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
 ﻿/* assim: */
-// using ExemplosFundamentos.Models;
-// Pessoa p = new Pessoa();
+using ExemplosFundamentos.Models;
+
+string apresentacao = "Olá, seja bem-vindo";
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.80M;
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Altura: " + altura.ToString("0.00"));
+Console.WriteLine("Preço: " + preco);
+Console.WriteLine("Quantidade e Condição: " + quantidade + " - " + condicao);
+
+
+
+
+
+Pessoa pessoa1 = new Pessoa();
 
 /* ou assim: */
-ExemplosFundamentos.Models.Pessoa pessoa1 = new ExemplosFundamentos.Models.Pessoa();
+// ExemplosFundamentos.Models.Pessoa pessoa1 = new ExemplosFundamentos.Models.Pessoa();
 
 pessoa1.Nome = "Vinicius";
 pessoa1
 
-
 .Idade = 38;
-pessoa1.Apresentar();
+// pessoa1.Apresentar();
