3325b6396def1ca2e1d11d770b379219658177d0
vinicius <vmm_7@hotmail.com>
2022-12-29 13:30:37 -0300

    operador IF e modo Debug em ação

--- Program.cs
+++ Program.cs
@@ -1,15 +1,38 @@
 ﻿/* assim: */
 using ExemplosFundamentos.Models;
 
+// Operador Condicional
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+if (possivelVenda)
+{
+    Console.WriteLine("Venda realizada.");
+}
+else
+{
+    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+}
+
+
+
+
+
+
 
 // O TryParse EVITA quebrar a execução do programa caso dê algum erro de conversão!
-string a = "15--c";
+// string a = "15--c";
 // int b = 110;
 // int.TryParse(a, out b); OU posso fazer assim também:
-int.TryParse(a, out int b);
+// int.TryParse(a, out int b);
 
-Console.WriteLine(b);
-Console.WriteLine("Conversão realizada com sucesso!");
+// Console.WriteLine(b);
+// Console.WriteLine("Conversão realizada com sucesso!");
 
 
 
