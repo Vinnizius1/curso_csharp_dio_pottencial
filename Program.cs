6be73a511de1034895d2bef80fd19f564c282679
vinicius <vmm_7@hotmail.com>
2022-12-30 13:51:10 -0300

    switch case

--- Program.cs
+++ Program.cs
@@ -1,23 +1,52 @@
-﻿/* assim: */
-using ExemplosFundamentos.Models;
+﻿// using ExemplosFundamentos.Models;
+
+
+// Switch case
+Console.WriteLine("Digite uma letra:");
+string letra = Console.ReadLine();
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        System.Console.WriteLine("Vogal");
+        break;
+
+    default:
+        System.Console.WriteLine("Não é uma vogal");
+        break;
+}
+
+
+
+
+
+
 
 // Operador Condicional
-int quantidadeEmEstoque = 10;
-int quantidadeCompra = 4;
-bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+// int quantidadeEmEstoque = 10;
+// int quantidadeCompra = 0;
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
 
-Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
-Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
-Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+// Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
-if (possivelVenda)
-{
-    Console.WriteLine("Venda realizada.");
-}
-else
-{
-    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
-}
+// if (quantidadeCompra == 0)
+// {
+//     Console.WriteLine("Venda inválida.");
+// }
+// else if (possivelVenda)
+// {
+//     Console.WriteLine("Venda realizada.");
+// }
+// else
+// {
+//     Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+// }
 
 
 
@@ -93,13 +122,13 @@ else
 
 
 
-Pessoa pessoa1 = new Pessoa();
+// Pessoa pessoa1 = new Pessoa();
 
 /* ou assim: */
 // ExemplosFundamentos.Models.Pessoa pessoa1 = new ExemplosFundamentos.Models.Pessoa();
 
-pessoa1.Nome = "Vinicius";
-pessoa1
+// pessoa1.Nome = "Vinicius";
+// pessoa1
 
-.Idade = 38;
+// .Idade = 38;
 // pessoa1.Apresentar();
