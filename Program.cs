78e30e6a4ce56009425b4c7b0f20c217466fe85c
vinicius <vmm_7@hotmail.com>
2023-01-11 21:58:28 -0300

    Criando um método

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using ExemploExplorando.Models;
 
 // Instanciando uma classe Pessoa
+// Temos então uma variável "p1" do tipo "Pessoa"
 Pessoa p1 = new Pessoa();
+
+p1.Nome = "Vinicius";
+p1.Idade = 38;
+p1.Apresentar();
